3e02565fef51dbe83d29f8a03c565bf3d73944fe
J.D <romulas80@gmail.com>
2017-10-26 12:15:51 -0500

    Delete authenticate.java

--- authenticate.java
+++
@@ -1,6 +0,0 @@
-Authenticator.setDefault(new Authenticator() {
-     protected PasswordAuthentication getPasswordAuthentication() {
-       return new PasswordAuthentication(username, password.toCharArray());
-     
-   });
- }
